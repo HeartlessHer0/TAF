e0e522df18df9b46482fae6e8bb437e5de7a5bc9
Sergey Zhigar <jedai38@gmail.com>
2022-04-18 22:08:58 +0300

    First Selenium Tests

--- src/main/java/SimpleDriver.java
+++ src/main/java/SimpleDriver.java
@@ -1,13 +1,10 @@
-package Tests;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
-public class FirstTest {
-    @Test
-    public void browserTest(){
+public class SimpleDriver {
+    public WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver", "E:\\AQA18\\TAF\\src\\test\\resources\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        return driver;
     }
 }
